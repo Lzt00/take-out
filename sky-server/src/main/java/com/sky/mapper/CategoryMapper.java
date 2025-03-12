bb41920f063faecd955a1114235a86486a930bce
Lzt00 <1845305261@qq.com>
2025-03-12 22:36:40 +0800

    完成员工管理模块和分类管理模块

---
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -0,0 +1,30 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Mapper
+public interface CategoryMapper {
+    Page<Category> page(CategoryPageQueryDTO categoryPageQueryDTO);
+
+    @Insert("insert into category(type,name,sort,status,create_time,update_time,create_user,update_user)" +
+            "values(#{type},#{name},#{sort},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    void inset(Category category);
+
+    void update(Category category);
+
+    @Delete("delete from category where id = #{id}")
+    void delete(Long id);
+
+    @Select("select * from category where type=#{type}")
+    List<Category> selectByType(String type);
+}
